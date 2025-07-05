dfe52c21c9a0368dec73e76191f9af625196edf0
satyamcloud <113009039+satyamcloud@users.noreply.github.com>
2025-07-05 15:28:17 +0530

    first version

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host="database-1.c2f46y28s2vw.us-east-1.rds.amazonaws.com",
+    user="admin",
+    password="Test#123",
+    database="myprojectdb"
+)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/users')
+def users():
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name, email FROM users")
+    data = cursor.fetchall()
+    return render_template('users.html', users=data)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
